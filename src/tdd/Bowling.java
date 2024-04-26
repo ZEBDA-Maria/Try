d29439a7fe200189e48a03ca188501c41b5c680d
Maria Zebda <zbm4548a@ensfsi.onmicrosoft.com>
2024-04-26 09:55:17 +0200

    changement

--- src/tdd/Bowling.java
+++ src/tdd/Bowling.java
@@ -2,4 +2,7 @@ package tdd;
 
 public class Bowling {
 
+	public String helloworld(String nom) {
+		return "hello world "+ nom;
+	}
 }
--- test/tdd/BowlingTest.java
+++ test/tdd/BowlingTest.java
@@ -1,6 +1,21 @@
 package tdd;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class BowlingTest {
- private int nb;
- private int a;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+public class BowlingTest{
+	Bowling game;
+	
+	@BeforeEach
+	void init() {
+		game= new Bowling();
+	
+	}
+	@Test
+	void testt() {
+		assertEquals("hello world maria",game.helloworld("maria"));
+	}
+	
 }
+
+
